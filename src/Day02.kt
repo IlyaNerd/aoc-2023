c35fecb697704aac3b787a1316d4da8783cdaa9a
ilya.aliaksandrovich <ilya.aliaksandrovich@sedex.com>
2023-12-01 10:26:36 +0100

    day 2 template

---
+++ src/Day02.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        TODO()
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part2(testInput).also { it.println() } == 281)
+
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}

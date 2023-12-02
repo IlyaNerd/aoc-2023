caa3ab3f10da230bedc41ebbb4cb703338def886
ilya.aliaksandrovich <ilya.aliaksandrovich@sedex.com>
2023-12-02 09:57:08 +0100

    day 2 part 1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,15 +1,66 @@
 fun main() {
+    data class CubeSet(
+        val blue: Int,
+        val green: Int,
+        val red: Int,
+    )
+
+    val totalCubes = CubeSet(
+        red = 12,
+        green = 13,
+        blue = 14,
+    )
+
+    data class Game(
+        val index: Int,
+        val sets: List<CubeSet>
+    )
+
+    fun String.toGameSet(): CubeSet {
+        val regex = Regex("(\\d+) (blue|green|red)")
+        val set = this.split(",")
+            .map {
+                val groupValues = regex.find(it.trim())?.groupValues ?: error("No matches in $this")
+                val number = groupValues[1].toInt()
+                val color = groupValues[2]
+                number to color
+            }
+        return CubeSet(
+            blue = set.firstOrNull { it.second == "blue" }?.first ?: 0,
+            green = set.firstOrNull { it.second == "green" }?.first ?: 0,
+            red = set.firstOrNull { it.second == "red" }?.first ?: 0,
+        )
+    }
+
+    fun String.toGame(): Game {
+        val indexRegex = Regex("Game (\\d+): ")
+        val index = indexRegex.find(this)?.groupValues?.get(1)?.toInt() ?: error("No index in $this")
+        val sets = this.replaceFirst(indexRegex, "")
+            .split(";")
+            .map { it.toGameSet() }
+        return Game(index, sets)
+    }
+
+    fun Game.isValid(): Boolean {
+        return this.sets.all {
+            it.red <= totalCubes.red && it.green <= totalCubes.green && it.blue <= totalCubes.blue
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        TODO()
+        return input
+            .map { line -> line.toGame() }
+            .filter { it.isValid() }
+            .sumOf { it.index }
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        return input.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part2(testInput).also { it.println() } == 281)
+    check(part1(testInput).also { it.println() } == 8)
 
     val input = readInput("Day02")
     part1(input).println()

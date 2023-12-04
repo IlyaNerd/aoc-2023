56ae3a446d8a5ebd91fbbdcb8384ae84f65a0490
ilya.aliaksandrovich <ilya.aliaksandrovich@sedex.com>
2023-12-04 09:20:44 +0100

    day 4 part 1

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,6 +1,25 @@
+import kotlin.math.pow
+
 fun main() {
+    val cardNumberRegex = Regex("Card\\s+(\\d+):")
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val (winningNumbers, myNumbers) = line.replace(cardNumberRegex, "")
+                .trim()
+                .split("|")
+                .map { set -> set.split(" ").filter { it.isNotBlank() }.map { it.trim().toInt() } }
+
+            val wins = myNumbers
+                .filter { it in winningNumbers }
+
+            val result = when {
+                wins.isEmpty() -> 0.0
+                wins.size == 1 -> 1.0
+                else -> 2.0.pow((wins.size - 1).toDouble())
+            }
+            result
+        }.toInt()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +28,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    check(part1(testInput).also { it.println() } == 281)
+    check(part2(testInput).also { it.println() } == 30)
 
     val input = readInput("Day04")
     part1(input).println()

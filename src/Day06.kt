aa0aba1cff7b53ab036991ef303e331932cc3671
ilya.aliaksandrovich <ilya.aliaksandrovich@sedex.com>
2023-12-06 09:21:23 +0100

    day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,33 +1,38 @@
 fun main() {
     data class TimeDistance(
-        val time: Int,
-        val minDistance: Int,
+        val time: Long,
+        val minDistance: Long,
     )
+
+    fun TimeDistance.countPossibleWins() = (1..<time).count { holdTime ->
+        val leftOverTime = time - holdTime
+        val distanceTraveled = leftOverTime * holdTime
+        distanceTraveled > minDistance
+    }
 
     fun part1(input: List<String>): Int {
         val (times, distances) = input.map {
-            it.substringAfter(':').split(' ').filter { it.isNotBlank() }.map { it.toInt() }
+            it.substringAfter(':').split(' ').filter { it.isNotBlank() }.map { it.toLong() }
         }
         val timeDistances = times.zip(distances) { a, b -> TimeDistance(a, b) }
         return timeDistances
-            .map {
-                val timeRange = 1..<it.time
-                timeRange.count {holdTime ->
-                    val leftOverTime = it.time - holdTime
-                    val distanceTraveled = leftOverTime * holdTime
-                    distanceTraveled > it.minDistance
-                }
-            }
+            .map { it.countPossibleWins() }
             .reduce { a, b -> a * b }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val timeDistance = input.map {
+            it.substringAfter(':').replace(" ", "").toLong()
+        }.let {
+            check(it.size == 2)
+            TimeDistance(it[0], it[1])
+        }
+        return timeDistance.countPossibleWins()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
-    check(part1(testInput).also { it.println() } == 288)
+    check(part2(testInput).also { it.println() } == 71503)
 
     val input = readInput("Day06")
     part1(input).println()

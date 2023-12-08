92582ecdccf494fa63f0c5f80fc5af4b3eea5993
ilya.aliaksandrovich <ilya.aliaksandrovich@sedex.com>
2023-12-08 10:09:22 +0100

    day 8 part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,12 +1,15 @@
 fun main() {
+    val lineRegex = Regex("^(.+) = \\((.+), (.+)\\)$")
+
+    fun List<String>.toPairs() = associate { line ->
+        val (_, start, left, right) = lineRegex.find(line)!!.groupValues
+        start to (left to right)
+    }
+
     fun part1(input: List<String>): Int {
         val steps = input[0].also { it.println() }
-        val lineRegex = Regex("^(.+) = \\((.+), (.+)\\)$")
         val instructions = input.drop(2)
-            .associate { line ->
-                val (_, start, left, right) = lineRegex.find(line)!!.groupValues
-                start to (left to right)
-            }
+            .toPairs()
             .onEach { it.println() }
         var key = "AAA"
         var count = 0
@@ -24,12 +27,31 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val steps = input[0].also { it.println() }
+        val instructions = input.drop(2)
+            .toPairs()
+            .onEach { it.println() }
+        val keys = instructions.keys.filter { it.endsWith("A") }.toMutableList()
+            .also { it.println() }
+        var count = 0
+        while (true) {
+            steps.forEach { step ->
+                if (keys.all { it.endsWith("Z") }) return count
+                count++
+                keys.replaceAll { key ->
+                    if (step == 'L') {
+                        instructions[key]!!.first
+                    } else {
+                        instructions[key]!!.second
+                    }
+                }
+            }
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
-    check(part1(testInput).also { it.println() } == 6)
+    check(part2(testInput).also { it.println() } == 6)
 
     val input = readInput("Day08")
     part1(input).println()

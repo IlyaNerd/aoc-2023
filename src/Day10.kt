34db5094ea16edb3648bcd3a0a57788936ca86b9
ilya.aliaksandrovich <ilya.aliaksandrovich@sedex.com>
2023-12-10 10:22:41 +0100

    day 10 part 1

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,6 +1,118 @@
+import Direction.*
+
+enum class Direction {
+    TOP, BOTTOM, LEFT, RIGHT
+}
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val startLineIndex = input.indexOfFirst { it.contains('S') }
+        val startLine = input[startLineIndex]
+        val startIndex = startLine.indexOf('S')
+
+        var from = TOP
+        var i = startLineIndex
+        var j = startIndex
+
+        when {
+            input.getOrNull(i - 1)?.get(j) in listOf('F', '7', '|') -> {
+                i--
+                from = BOTTOM
+            }
+            input.getOrNull(i + 1)?.get(j) in listOf('L', 'J', '|') -> {
+                i++
+                from = TOP
+            }
+            input[i].getOrNull(j - 1) in listOf('F', 'L', '-') -> {
+                j--
+                from = RIGHT
+            }
+            input[i].getOrNull(j + 1) in listOf('7', 'J', '-') -> {
+                j++
+                from = LEFT
+            }
+            else -> error("unreachable")
+        }
+
+
+        var steps = 0
+        while (true) {
+            println("$i $j $from")
+            when (input[i][j]) {
+                '|' -> when (from) {
+                    TOP -> i++
+                    BOTTOM -> i--
+                    LEFT,
+                    RIGHT -> error("unreachable $i $j")
+                }
+
+                '-' -> when (from) {
+                    LEFT -> j++
+                    RIGHT -> j--
+                    TOP,
+                    BOTTOM -> error("unreachable $i $j")
+                }
+
+                'L' -> when (from) {
+                    TOP -> {
+                        j++
+                        from = LEFT
+                    }
+                    RIGHT -> {
+                        i--
+                        from = BOTTOM
+                    }
+                    LEFT,
+                    BOTTOM -> error("unreachable $i $j")
+                }
+
+                'J' -> when (from) {
+                    TOP -> {
+                        j--
+                        from = RIGHT
+                    }
+                    LEFT -> {
+                        i--
+                        from = BOTTOM
+                    }
+                    RIGHT,
+                    BOTTOM -> error("unreachable $i $j")
+                }
+
+                '7' -> when (from) {
+                    BOTTOM -> {
+                        j--
+                        from = RIGHT
+                    }
+                    LEFT -> {
+                        i++
+                        from = TOP
+                    }
+                    TOP,
+                    RIGHT -> error("unreachable $i $j")
+                }
+
+                'F' -> when (from) {
+                    BOTTOM -> {
+                        j++
+                        from = LEFT
+                    }
+                    RIGHT -> {
+                        i++
+                        from = TOP
+                    }
+                    TOP,
+                    LEFT -> error("unreachable $i $j")
+                }
+
+                '.' -> error("Should not be here $i, $j")
+                'S' -> break
+                else -> error("unreachable $i $j")
+            }
+            steps++
+        }
+
+        return (steps + 1) / 2
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +121,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
-    check(part1(testInput).also { it.println() } == 281)
+    check(part1(testInput).also { it.println() } == 8)
 
-    val input = readInput("Day11")
+    val input = readInput("Day10")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }

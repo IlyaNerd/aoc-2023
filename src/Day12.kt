d9c81f2424efdec022a238a86fad9ba65aa387ee
ilya.aliaksandrovich <ilya.aliaksandrovich@sedex.com>
2023-12-12 13:57:46 +0100

    day 12 part 1

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,6 +1,49 @@
 fun main() {
+    data class Springs(val seq: String, val faulty: List<Int>) {
+        val faultyRegex = faulty.mapIndexed { i, it ->
+            if (i != faulty.size - 1) "#{$it}\\.+"
+            else "#{$it}\\.*"
+        }.joinToString(prefix = "^\\.*", postfix = "$", separator = "")
+            .let(::Regex)
+    }
+
+    fun Springs.getArrangements(): Int {
+        val regex = this.faultyRegex
+            .also { println(it) }
+
+        val results = mutableSetOf<String>()
+
+        fun generate(content: CharArray, i: Int) {
+            if (content.none { it == '?' }) {
+                val result = String(content)
+                if (!results.contains(result) && regex.matches(result)) {
+                    results.add(result)
+                }
+                return
+            }
+
+            (i..<content.size).forEach { j ->
+                if (content[j] == '?') {
+                    generate(content.copyOf().also { it[j] = '.' }, j + 1)
+                    generate(content.copyOf().also { it[j] = '#' }, j + 1)
+                } else {
+                    generate(content, j + 1)
+                }
+            }
+        }
+
+        generate(seq.toCharArray(), 0)
+
+        return results.size
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { line ->
+                val (springs, numbers) = line.split(' ')
+                Springs(springs, numbers.split(',').map { it.trim().toInt() })
+            }
+            .sumOf { a -> a.getArrangements().also { println(a to it) } }
     }
 
     fun part2(input: List<String>): Int {

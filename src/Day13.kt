144ff2434c1d3030b542215ed7a56aa8e30e3123
ilya.aliaksandrovich <ilya.aliaksandrovich@sedex.com>
2023-12-13 22:55:29 +0100

    day 13 part 1

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,6 +1,35 @@
 fun main() {
+    fun List<String>.toColumns(): List<String> {
+        return (0..<first().length).map { i -> this.map { it[i] }.toCharArray().let(::String) }
+    }
+
+    fun List<String>.findPerfectReflection(): Int? {
+        val s = (1..<this.size).find { i ->
+            val left = this.take(i).reversed()
+            val right = this.drop(i)
+            val zip = left.zip(right)
+            zip.all { (a, b) -> a == b }
+        }
+        return s
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .fold(mutableListOf<MutableList<String>>()) { acc, line ->
+                if (acc.isEmpty()) acc.add(mutableListOf())
+                if (line.isBlank()) acc.add(mutableListOf())
+                else acc.last().add(line)
+                acc
+            }
+            .sumOf { group ->
+                val rowReflection = group.findPerfectReflection()
+                if (rowReflection != null) {
+                    rowReflection * 100
+                } else {
+                    val colReflection = group.toColumns().findPerfectReflection()
+                    colReflection ?: error("smth went wrong \n${group.joinToString("\n")}\n\n${group.toColumns().joinToString("\n")}")
+                }
+            }
     }
 
     fun part2(input: List<String>): Int {

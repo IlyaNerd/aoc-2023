3ade969be07908dceddeba6a504afaf28786a5d3
ilya.aliaksandrovich <ilya.aliaksandrovich@sedex.com>
2023-12-14 09:16:41 +0100

    day 14 part 1

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,6 +1,34 @@
 fun main() {
+    fun moveUp(input: List<CharArray>, i: Int, j: Int) {
+        if (i == 0) return
+        val c = input[i][j]
+        if (c == 'O') {
+            if (input[i - 1][j] == '.') {
+                input[i - 1][j] = 'O'
+                input[i][j] = '.'
+                moveUp(input, i - 1, j)
+            }
+        }
+    }
+
+    fun moveRocksUp(input: List<String>): List<CharArray> {
+        val newInput = input.map { it.toCharArray() }
+
+        for (i in 1 until newInput.size) {
+            for (j in 0 until newInput[i].size) {
+                moveUp(newInput, i, j)
+            }
+        }
+        return newInput
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return moveRocksUp(input)
+//            .onEach { println(String(it)) }
+            .reversed()
+            .map { line -> line.count { it == 'O' } }
+            .mapIndexed { i, rocks -> rocks * (i + 1) }
+            .sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +37,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day14_test")
-    check(part1(testInput).also { it.println() } == 136)
+    check(part2(testInput).also { it.println() } == 64)
 
     val input = readInput("Day14")
     part1(input).println()

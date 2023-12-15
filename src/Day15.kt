e0d2d61733da72c3efa719323e1700047e26b279
ilya.aliaksandrovich <ilya.aliaksandrovich@sedex.com>
2023-12-15 09:24:15 +0100

    day 15 part 1

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,6 +1,19 @@
 fun main() {
+    fun String.buildHash(): Int {
+        var sum = 0
+        this.forEach { c ->
+            sum += c.code
+            sum *= 17
+            sum = sum.rem(256)
+        }
+        return sum
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.flatMap { it.split(',') }
+            .sumOf { str ->
+                str.buildHash()
+            }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +22,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day15_test")
-    check(part1(testInput).also { it.println() } == 52)
+    check(part1(testInput).also { it.println() } == 1320)
 
     val input = readInput("Day15")
     part1(input).println()

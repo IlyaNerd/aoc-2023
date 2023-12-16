6f746515b28f3b26621b9119b690f25f90016dc9
ilya.aliaksandrovich <ilya.aliaksandrovich@sedex.com>
2023-12-16 14:42:22 +0100

    day 13 part 2

--- src/Day13.kt
+++ src/Day13.kt
@@ -13,14 +13,16 @@ fun main() {
         return s
     }
 
+    fun List<String>.toGroups() = fold(mutableListOf<MutableList<String>>()) { acc, line ->
+        if (acc.isEmpty()) acc.add(mutableListOf())
+        if (line.isBlank()) acc.add(mutableListOf())
+        else acc.last().add(line)
+        acc
+    }
+
     fun part1(input: List<String>): Int {
         return input
-            .fold(mutableListOf<MutableList<String>>()) { acc, line ->
-                if (acc.isEmpty()) acc.add(mutableListOf())
-                if (line.isBlank()) acc.add(mutableListOf())
-                else acc.last().add(line)
-                acc
-            }
+            .toGroups()
             .sumOf { group ->
                 val rowReflection = group.findPerfectReflection()
                 if (rowReflection != null) {
@@ -32,13 +34,56 @@ fun main() {
             }
     }
 
+    fun List<String>.findPerfectReflectionWithSmudges(): Int? {
+        val original = findPerfectReflection()
+        var smudge = Point(0, 0)
+        val yMax = first().length - 1
+        val xMax = size - 1
+        while (smudge.x <= xMax && smudge.y <= yMax) {
+            val list = this.toMutableList()
+            list[smudge.x] = list[smudge.x]
+                .toCharArray()
+                .also { it[smudge.y] = if (it[smudge.y] == '#') '.' else '#' }
+                .let(::String)
+
+            val reflection = list.findPerfectReflection()
+            if (reflection != null && original == null) return reflection
+            else if (reflection != null && reflection != original) {
+                return reflection
+            } else {
+                var x = smudge.x
+                var y = smudge.y
+                if (smudge.y == yMax) {
+                    y = 0
+                    x++
+                } else {
+                    y++
+                }
+                smudge = Point(x, y)
+            }
+        }
+        return null
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .toGroups()
+            .map { group ->
+                val rowReflection = group.findPerfectReflectionWithSmudges()
+                if (rowReflection != null) {
+                    rowReflection * 100
+                } else {
+                    val colReflection = group.toColumns().findPerfectReflectionWithSmudges()
+                    colReflection ?: error("smth went wrong \n${group.joinToString("\n")}\n\n${group.toColumns().joinToString("\n")}")
+                }
+            }
+            .onEach { println("value $it") }
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day13_test")
-    check(part1(testInput).also { it.println() } == 405)
+    check(part2(testInput).also { it.println() } == 400)
 
     val input = readInput("Day13")
     part1(input).println()
